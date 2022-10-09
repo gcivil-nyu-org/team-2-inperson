c99819ef1156d70289a73dc6b65932c7da6384a2
yalexublue <yalexu.blue@gmail.com>
2022-10-09 15:47:35 -0400

    added dimension prop

---
+++ src/frontend/shortlist/src/components/school/simple/SimDimensions.vue
@@ -0,0 +1,45 @@
+<script>
+import SimDimension from "../simple/SimDimension.vue"
+
+
+
+export default {
+  name: "SimDimensions",
+  components: {SimDimension},
+  setup() {
+    const dimensions = [
+      {text: "Dimension1:", href2: "http://www.google.com", color: "#FF00FF"},
+      {text: "Dimension2:", href2: "http://www.google.com"},
+    ];
+    return {dimensions};
+  }
+}
+</script>
+
+<template>
+  <div class="sim-dimensions-container">
+    <template v-for="dimension in dimensions" :key="dimension">
+      <SimDimension :text="dimension.text" :href2="dimension.href2" :color="dimension.color"/>
+    </template>
+
+    <!--
+    <NavLink text="Home" href2="http://www.google.com" color="blue"/>
+    -->
+  </div>
+</template>
+
+<style scoped>
+.sim-dimensions-container {
+  font-size: 20px;
+  width: 480px;
+  height: auto;
+  
+
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-evenly;
+  
+}
+
+
+</style>
