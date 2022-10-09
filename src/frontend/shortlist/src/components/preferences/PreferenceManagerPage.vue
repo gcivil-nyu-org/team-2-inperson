a5c44a0be354c27f683d8668ff3f5aed5f257755
Daniel Walt <daniel.r.walt@gmail.com>
2022-10-09 17:00:57 -0400

    wired form v-model for pref manager

---
+++ src/frontend/shortlist/src/components/preferences/PreferenceManagerPage.vue
@@ -0,0 +1,34 @@
+<script>
+import { inject } from 'vue'
+export default {
+  name: "PreferenceManagerPage",
+  props: ["id", "subtitle"],
+  setup() {
+    const selectedPage = inject("selectedPage");
+    
+    return {
+      selectedPage
+    }
+    
+  }
+}
+</script>
+
+<template>
+  <div v-show="id == selectedPage">
+    <div class="pref-section-header">{{subtitle}}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.pref-section-header{
+  font-size: 20px;
+  width: 100%;
+  margin-top: 15px;
+  padding: 10px;
+  color: white;
+  background-color: green;
+
+}
+</style>
--- src/frontend/shortlist/src/components/preferences/PreferenceMultiSelect.vue
+++ src/frontend/shortlist/src/components/preferences/PreferenceMultiSelect.vue
@@ -2,10 +2,20 @@
 export default {
   name: "PreferenceMultiSelect",
   props: [
-    "questionId",
-    "questionText",
-    "questionOptions",
-  ]
+    "question",
+    "modelValue",
+  ],
+  emits: ["update:modelValue"],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue
+      },
+      set(value) {
+        this.$emit("update:modelValue", value)
+      }
+    }
+  }
 }
 </script>
 
@@ -13,14 +23,28 @@ export default {
   <!--{{responseId}}-->
   <div class="pref-q-container">
     <div class="pref-q-name">
+<<<<<<< HEAD
       {{questionId}}
+=======
+      {{question.Name}}
+>>>>>>> 0d8011d (wired form v-model for pref manager)
     </div>
     <div class="pref-q-question">
-      {{questionText}}
+      {{question.Text}}
     </div>
+<<<<<<< HEAD
     <template v-for="opt, optindex in questionOptions" :key="optindex">
       <input type="checkbox" :id="optindex+''+opt" :name="optindex+''+opt" :value="opt">
       <label class="pref-opt-label" :for="optindex+''+opt"> {{opt}}</label><br>
+=======
+    <template v-for="opt, optindex in question.Options" :key="optindex">
+      <input type="checkbox" 
+        :value="opt"
+        :name="optindex" 
+        v-model="value">
+      
+      <label class="pref-opt-label" :for="optindex"> {{opt}}</label><br>
+>>>>>>> 0d8011d (wired form v-model for pref manager)
     </template>
   </div>
 </template>
@@ -29,6 +53,7 @@ export default {
 .pref-q-container {
   padding: 10px;
   width: 100%;
+  min-width: 450px;
 }
 .pref-q-name {
   width: 100%;
