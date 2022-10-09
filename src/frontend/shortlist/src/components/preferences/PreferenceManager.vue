1788ea1bd039e9c86502c62a018a1d2eab49a520
Daniel Walt <daniel.r.walt@gmail.com>
2022-10-09 12:54:29 -0400

    initial components for preference manager / quizzes

---
+++ src/frontend/shortlist/src/components/preferences/PreferenceManager.vue
@@ -0,0 +1,92 @@
+<script>
+export default {
+  name: "PreferenceManager",
+  props: ["title", "subtitle"],
+
+  setup() {
+    const actions = {
+      back: {
+        text: "Back",
+        enabled: true,
+        type: "button",
+        action: false
+      },
+      next: {
+        text: "Next",
+        enabled: true,
+        type: "button",
+        action: false
+      },
+      submit: {
+        text: "Submit",
+        enabled: false,
+        type: "button",
+        action: false
+      }
+    }
+    
+    return {actions}
+  },
+  
+}
+</script>
+
+<template>
+  <div class="pref-container">
+    <div class="pref-title">
+      {{title}}<br>
+      <span style="font-size: 20px;">{{subtitle}}</span>
+    </div>
+    
+    <div class="pref-main">
+      <slot></slot>
+    </div>
+    <div style="flex-grow: 1;">&nbsp;</div>
+    <div class="pref-actions">
+      <template v-for="action in actions" :key="action">
+        <button v-if="action.enabled" @click="action.action">{{action.text}}</button>
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pref-container {
+  width: 100%;
+  min-height: 400px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: baseline;
+}
+
+.pref-title {
+  width: 100%;
+  font-family: "Cabin Sketch", cursive;
+}
+
+.pref-main {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: baseline;
+}
+
+.pref-actions {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.pref-actions button {
+  padding: 20px;
+  background-color: green;
+}
+
+
+</style>
+
---
+++ src/frontend/shortlist/src/components/preferences/PreferenceMultiSelect.vue
@@ -0,0 +1,52 @@
+<script>
+
+
+export default {
+  name: "PreferenceMultiSelect",
+  props: [
+    "responseId",
+    "questionName",
+    "questionText",
+    "questionOptions",
+  ]
+}
+
+</script>
+
+<template>
+  <!--{{responseId}}-->
+  <div class="pref-q-container">
+    <div class="pref-q-name">
+      {{questionName}}
+    </div>
+    <div class="pref-q-question">
+      {{questionText}}
+    </div>
+    <template v-for="opt, optindex in questionOptions" :key="optindex">
+      <input type="checkbox" :id="responseId" :name="responseId + '.' + optindex" :value="optindex">
+      <label class="pref-opt-label" :for="responseId + '.' + optindex"> {{opt}}</label><br>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.pref-q-container {
+  padding: 10px;
+  width: 100%;
+}
+.pref-q-name {
+  width: 100%;
+  font-size: 24px;
+  font-weight: 500;
+}
+
+.pref-q-question {
+  width: 100%;
+  font-size: 18px;
+}
+
+.pref-opt-label {
+  font-size: 12px;
+}
+
+</style>
