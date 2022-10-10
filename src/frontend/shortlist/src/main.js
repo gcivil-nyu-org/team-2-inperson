a04a40b9f3c88ae22de20b0720b793443f16418a
Pooja Patel <poojapatel@nyu.edu>
2022-10-09 23:03:07 -0400

    Updated main.js for bootstrap

--- src/frontend/shortlist/src/main.js
+++ src/frontend/shortlist/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 const app = createApp(App);
 
