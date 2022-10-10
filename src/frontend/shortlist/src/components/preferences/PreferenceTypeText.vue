94f2fc76b8e452b0300064af621cce15652049de
Daniel Walt <daniel.r.walt@gmail.com>
2022-10-10 13:48:45 -0400

    fix text input styling to match dropdown

--- src/frontend/shortlist/src/components/preferences/PreferenceTypeText.vue
+++ src/frontend/shortlist/src/components/preferences/PreferenceTypeText.vue
@@ -21,14 +21,10 @@ export default {
 
 <template>
   <div class="pref-q-container">
-    <div class="pref-q-name">
-      {{question.Name}}
-    </div>
-    <div class="pref-q-question">
-      {{question.Text}}
-    </div>
+    <div class="pref-q-name">{{question.Name}}</div>
+    <div class="pref-q-question">{{question.Text}}</div>
     <div>
-      <input type="text" 
+      <input type="text" class="pref-input-text"
         :name="question.Name" 
         v-model="value">
     </div>
@@ -42,6 +38,7 @@ export default {
   font-family: 'Klee One', cursive;
   font-weight: bold;
 }
+
 .pref-q-name {
   width: 100%;
   font-size: 24px;
@@ -54,22 +51,17 @@ export default {
   font-size: 18px;
 }
 
-.pref-opt-label {
-  font-size: 12px;
-}
-
-.text{
+.pref-input-text {
   width: 100%;
-  padding: 10px 0;
-  background: none;
-  border: none;
-  border-bottom: 1px solid #666;
+  padding: 8px 5px;
+  background: #ebf3e6;
+  border: 1px solid #008037;
+  border-radius: 5px;
   color: black;
   font-weight: bold;
   font-size: 15px;
   font-family: 'Aleo', serif;
   outline: none;
   transition: border-color .2s;
-  font-family: 'Aleo', serif;
 }
 </style>
