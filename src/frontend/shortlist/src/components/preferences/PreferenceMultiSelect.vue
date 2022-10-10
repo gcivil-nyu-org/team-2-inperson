9492a2e707eeb649517fe9c30bf8135f318c6df8
Pooja Patel <poojapatel@nyu.edu>
2022-10-09 23:06:05 -0400

    Updated Pref MultiSelect Template/html

--- src/frontend/shortlist/src/components/preferences/PreferenceMultiSelect.vue
+++ src/frontend/shortlist/src/components/preferences/PreferenceMultiSelect.vue
@@ -22,20 +22,11 @@ export default {
 <template>
   <div class="pref-q-container">
     <div class="pref-q-name">
-<<<<<<< HEAD
-      {{questionId}}
-=======
       {{question.Name}}
->>>>>>> 0d8011d (wired form v-model for pref manager)
     </div>
     <div class="pref-q-question">
       {{question.Text}}
     </div>
-<<<<<<< HEAD
-    <template v-for="opt, optindex in questionOptions" :key="optindex">
-      <input type="checkbox" :id="optindex+''+opt" :name="optindex+''+opt" :value="opt">
-      <label class="pref-opt-label" :for="optindex+''+opt"> {{opt}}</label><br>
-=======
     <template v-for="opt, optindex in question.Options" :key="optindex">
       <input type="checkbox" 
         :value="opt"
@@ -43,8 +34,8 @@ export default {
         v-model="value">
       
       <label class="pref-opt-label" :for="optindex"> {{opt}}</label><br>
->>>>>>> 0d8011d (wired form v-model for pref manager)
     </template>
+  </div>
   </div>
 </template>
 
