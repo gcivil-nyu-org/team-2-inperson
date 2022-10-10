9ba7b8978527ab4e3554ef7758d9fd9c7e451056
Simao (Alice) Chen <67714084+simaoalicechen@users.noreply.github.com>
2022-10-10 19:48:34 -0400

    Add Sidebar files and images

---
+++ src/frontend/shortlist/src/components/layout/sidebar/SimButton.vue
@@ -0,0 +1,29 @@
+<template>
+      <img id = "center" class="img-left-card" height = 20, width=10 src="../../../image/trash-can.png" 
+        alt="img-left-card" />
+</template>
+
+<script>
+export default {
+    name: "SimButton",
+  //   data() {
+  //   return {
+  //     disabled: false
+  //   }
+  // },
+}
+</script>
+<style scoped>
+.img-left-card {
+    height:60px;
+    width: 75px;
+    margin-left: 50%;
+    margin-right:50%;
+    margin-top:75%;
+    margin-bottom: 75%;
+}
+.center {
+  float: center;
+}
+
+</style>
