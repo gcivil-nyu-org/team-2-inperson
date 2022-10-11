7833b1dbc78e302f337b2d8f2cfeed96f2316af6
Pooja Patel <poojapatel@nyu.edu>
2022-10-11 17:23:23 -0400

    Updated Navbar with CSS

--- src/frontend/shortlist/src/components/layout/navigation/Navbar.vue
+++ src/frontend/shortlist/src/components/layout/navigation/Navbar.vue
@@ -32,15 +32,23 @@ export default {
 </script>
 
 <template>
-<div>
-    <nav>
-      <!--<router-link to="/home">Home</router-link>
-      <img class="nav-img" height = 20, width=10  src="../../image/shortlist.sm" alt="shortlist" />-->
-      <router-link class="spacing" v-for="routes in links" 
-      v-bind:key="routes.id"
-      :to="`${routes.page}`">{{routes.text}}</router-link>
+    <nav class="navbar navbar-expand-lg static-top" style="background-color:white;">
+      
+    <router-link to="/" class="reset-style"><img src="short-list-logo-light-navbar.png" style="width: 170px;
+    left: 15px;" alt="shortlist" /></router-link>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#shortlistmenu" aria-controls="shortlistmenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="shortlistmenu">
+        <ul class="navbar-nav ms-auto">
+            <router-link  class="nav-item" style="display:inline;text-decoration:none;" v-for="routes in links" 
+            v-bind:key="routes.id"
+            :to="`${routes.page}`"><li class="nav-link active" aria-current="page">{{routes.text}}</li></router-link>
+        </ul>
+      </div>
     </nav>
-  </div>
 </template>
 
 
@@ -48,11 +56,32 @@ export default {
 .spacing {
   margin-right: 10px;
 }
-.nav {
-    align: right;
-    display:flex;
-}
-.nav-img {
 
+.navbar{
+  font-family: "Cabin Sketch", cursive;
+  font-weight: bold;
 }
+.router-link-active {
+    border-radius: 70px;
+    border: 1px solid #067418;
+    align-items: center;
+    font-weight: 500;
+    box-shadow: rgba(44, 187, 99, .2) 0 -25px 18px -14px inset,rgba(44, 187, 99, .15) 0 1px 2px,rgba(44, 187, 99, .15) 0 2px 4px,rgba(44, 187, 99, .15) 0 4px 8px,rgba(44, 187, 99, .15) 0 8px 16px,rgba(44, 187, 99, .15) 0 16px 32px;
+    color: white;
+    cursor: pointer;
+    display: inline-block;
+    padding: 1px 10px;
+    text-align: center;
+    text-decoration: none;
+    transition: all 250ms;
+    border: 0;
+    font-size: px;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+  }
+
+  .reset-style, .reset-style * {
+        all: revert;
+    }
 </style>
