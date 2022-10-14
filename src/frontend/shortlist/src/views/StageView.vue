5be2f8ef500086d162980e702973455e9e0e173b
Simao (Alice) Chen <67714084+simaoalicechen@users.noreply.github.com>
2022-10-13 20:52:09 -0400

    Added View for reference

---
+++ src/frontend/shortlist/src/views/StageView.vue
@@ -0,0 +1,18 @@
+<script>
+import DetailCard from "../components/school/detail/DetailCard.vue";
+import SimCardDemo from "../components/school/simple/SimCardDemo.vue";
+export default {
+    name: "StageView",
+    components: { DetailCard, SimCardDemo }
+}
+</script>
+
+<template>
+    <div class="container">
+      <DetailCard></DetailCard>
+      <!-- <SimCardDemo></SimCardDemo> -->
+    </div>
+</template>
+
+<style scoped>
+</style>
