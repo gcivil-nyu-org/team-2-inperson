a013bfe0058a31372eb11cffe1e6f500916d2de3
Simao (Alice) Chen <67714084+simaoalicechen@users.noreply.github.com>
2022-10-15 17:41:25 -0400

    Added Detailed View Card

--- src/frontend/shortlist/src/components/school/detail/DetailCard.vue
+++ src/frontend/shortlist/src/components/school/detail/DetailCard.vue
@@ -1,45 +1,82 @@
 <script>
-/*
-import SimPic from "../SimpleView/SimPic.vue"
-import SimTitle from "../SimpleView/SimTitle.vue"
-*/
-import DetailProps from "../detail/DetailProps.vue"
-
-export default {
-  name: "DetailCard",
-  components: { DetailProps },
-}
-
+    export default {
+        name: "DetailCard",
+        props: ["schoolDetailData"],
+    }
 </script>
 
 <template>
-    <div class="detail-card-container">
-        <h4 class="detail-title">Detail View</h4>
-        <DetailProps></DetailProps>
+    <div class="card-container">
+        <div style="width: 100%; diplay: flex; flex-wrap: wrap; justify-content:flex-end; padding: 15px;">
+            <a href="https://stuy.enschool.org/"><div class="detail-school-name">
+                {{schoolDetailData.name}}
+            </div></a>
+            <div class="detail-school-borough">
+                {{schoolDetailData.borough}}
+            </div>
+        </div>
+        <!-- <div class="column_wrapper"> -->
+            <div class = "dimension-rows">
+                <template v-for="dimension in schoolDetailData.dimensions" :key="dimension">
+                    <div class="dimension-row">
+                        <div class="dimension-name">{{dimension.name}}</div>
+                        <div class="dimension-value">{{dimension.value}}</div>
+                    </div>
+                </template>
+            </div>
+        <!-- </div> -->
     </div>
-    
 </template>
 
 <style scoped>
-.detail-card-container {
-/* Old style
-    background-color: white;
-    border: 1px none;
-    margin-left:auto;
-    margin-right:auto;
-    border-radius: 8px;
-    box-shadow: 0px 4px 4px #00000040;
+
+.dimension-name {
+    font-family: "Aleo";
+    font-weight: bold;
+    font-size: 14px;
+    border-bottom: 1px solid #779886;
+}
+
+.dimension-rows {
+    width: 100%; 
+    display: flex; 
+    flex-wrap: wrap; 
+    justify-content: 
+    space-between; 
+    align-items: center; 
+    padding: 15px;
+}
+
+.dimension-value {
+    font-family: sans-serif;
+    font-size: 14px;
+    margin-left: 5px;
+}
+
+.dimension-row {
+    width: 10000%;
+    margin-bottom: 2px;
+}
+
+.detail-school-name {
     display: flex;
-    height: 480px;
-    width: 400px;
-    min-width: 300px;
-    flex-wrap: wrap;
-    justify-content: space-between;
     align-items: center;
-    */
+    justify-content: flex-end;
+    font-weight: bold;
+    font-size: 18px;
+    font-family: "Libre Baskerville" ;
+}
+
+.detail-school-borough {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 14px;
+}
+
+.card-container {
     width: 400px;
     height: 600px; 
-    /* align-self: center;  */
     margin: auto; 
     border-radius: 20px;
     padding: 40px;
@@ -48,13 +85,8 @@ export default {
     box-shadow: 14px 14px 20px #779886, -14px -14px 20px white;
     font-family: "Aleo";
 }
-.detail-title {
-    /* align-self: center; */
-    margin: auto; 
-    letter-spacing: 0;
-    margin-right: 1px;
-    min-height: 50px;
-    text-align: center;
-    width: 398px;
-}
+/* 
+.column_wrapper {
+  column-count: 2;
+} */
 </style>
