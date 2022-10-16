a805cedd67505ddaf004f999d89fda0eed09f5d5
yalexublue <yalexu.blue@gmail.com>
2022-10-16 18:56:46 -0400

    resolve pull conflict

--- src/frontend/shortlist/src/views/DemoView.vue
+++ src/frontend/shortlist/src/views/DemoView.vue
@@ -1,27 +1,18 @@
 <script>
-import SimpleBarView from "../components/layout/sidebar/SimpleBarView.vue";
-import SimCardDemo from "../components/layout/composite/CompositeView.vue";
-
+import CompositeView from "../components/layout/composite/CompositeView.vue";
 export default {
   components: {
-    SimpleBarView,
-    SimCardDemo
-  },
+    CompositeView,
+},
 }
 </script>
 
 <template>
   <h1>Week of 2022-10-13 (TBD)</h1>
   <!--List of demos<br>-->
-  <SimpleBarView>
-    <div class="middle-wrapper">
-      <SimCardDemo></SimCardDemo>
-    </div>
-  </SimpleBarView>
-
-  
+  <CompositeView>
+  </CompositeView>
 </template>
 
 <style scoped>
-
 </style>
