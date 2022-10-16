ee54c25912359c6843e48c31188161c65469a52d
Adelaide Zhang <adelaidemzhang@gmail.com>
2022-10-16 19:41:10 -0400

    Add List.vue component(child), ListBar.vue(parent)

---
+++ src/frontend/shortlist/src/components/layout/sidebar/List.vue
@@ -0,0 +1,29 @@
+<script>
+  export default {
+      name: "List",
+      props: ["listName", "listItems"],
+    }
+</script>
+
+<template>
+  <div class="list-object">
+    <h3>{{ listName }}</h3>
+    <p v-for="item in listItems">
+      {{ listItems.schName }}
+    </p>
+  </div>
+
+</template>
+
+<style>
+.list-object {
+  width: 90%;
+  height: 100px;
+  
+  border-radius: 10px;
+  padding: 5%;
+  background: #a26ba0;
+  box-shadow: 5px 5px 10px #779886, -5px -5px 10px white;
+  font-family: "Aleo";
+}
+</style>
