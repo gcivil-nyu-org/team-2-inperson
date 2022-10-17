b05db891aab459f18a9e693dd63816e736039273
Alice Chen <alicechen005@gmail.com>
2022-10-16 21:48:10 -0400

    Added everything in this branch

--- src/frontend/shortlist/src/views/StageView.vue
+++ src/frontend/shortlist/src/views/StageView.vue
@@ -1,5 +1,5 @@
 <script>
-import DetailCardDemo from "../components/school/detail/DetailCard.vue";
+import DetailCardDemo from "../components/school/detail/DetailCardDemo.vue";
 import SimCardDemo from "../components/school/simple/SimCardDemo.vue";
 export default {
     name: "StageView",
@@ -15,4 +15,12 @@ export default {
 </template>
 
 <style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
 </style>
