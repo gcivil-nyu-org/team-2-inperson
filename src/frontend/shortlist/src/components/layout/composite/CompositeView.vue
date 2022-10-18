d4ae2e3f8845bf366be1cf2ef459aacbb570ee32
Adelaide Zhang <adelaidemzhang@gmail.com>
2022-10-18 14:16:04 -0400

    Add description to detail card

--- src/frontend/shortlist/src/components/layout/composite/CompositeView.vue
+++ src/frontend/shortlist/src/components/layout/composite/CompositeView.vue
@@ -75,6 +75,10 @@ export default {
           value: "345 Chambers St, New York, NY 10282"
         },
         description: {
+          name: "School Description",
+          value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        },
+        rigorous_instruction: {
           name: "Rigorous Instruction Rating",
           value: "89%"
         },
