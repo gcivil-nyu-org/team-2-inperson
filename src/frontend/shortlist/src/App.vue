cb5b6a68bd9ff1783de59a8248a078664eb1bb78
Adelaide Zhang <adelaidemzhang@gmail.com>
2022-10-19 13:29:48 -0400

    Log version

--- src/frontend/shortlist/src/App.vue
+++ src/frontend/shortlist/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navbar from "./components/layout/navigation/Navbar.vue"
+
+console.log("Version B");
 </script>
 
 <template>
