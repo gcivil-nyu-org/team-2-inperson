bf87206df5e40750dd06f7697edf0ca4e8f7b86a
Daniel Walt <daniel.r.walt@gmail.com>
2022-10-22 14:02:39 +0000

    setup recommendation stack component

---
+++ src/frontend/shortlist/src/components/layout/recommendations/RecommendationStack.vue
@@ -0,0 +1,55 @@
+<script>
+import SchoolCard from "../../school/SchoolCard.vue";
+
+export default {
+  name: "RecommendationStack",
+  props: ["recommendations"],
+  components: { SchoolCard },
+  created() {
+    //constants for the offset factors
+    this.offsetFactor = 15;
+  },
+};
+</script>
+
+<template>
+  <div class="reco-stack-container">
+    <template v-for="(reco, idx) in recommendations" :key="idx">
+      <template v-if="idx == 0">
+        <div
+          class="reco-stack-grid-position"
+          :style="{
+            marginLeft:
+              offsetFactor * (recommendations.length - idx + 1) + 'px',
+            marginTop: offsetFactor * idx + 'px',
+            zIndex: 200 * (recommendations.length - idx + 1),
+          }"
+        >
+          <SchoolCard :schoolData="reco" />
+        </div>
+      </template>
+      <template v-else>
+        <div
+          class="reco-stack-grid-position"
+          :style="{
+            marginLeft:
+              offsetFactor * (recommendations.length - idx + 1) + 'px',
+            marginTop: offsetFactor * idx + 'px',
+            zIndex: 200 * (recommendations.length - idx + 1),
+          }"
+        >
+          <SchoolCard :schoolData="reco" />
+        </div>
+      </template>
+    </template>
+  </div>
+</template>
+
+<style>
+.reco-stack-container {
+  display: grid;
+}
+.reco-stack-grid-position {
+  grid-area: 1 / 1 / 1 / 1;
+}
+</style>
