294d43cda98125862204297260b4cddd1baf83d9
adelaidemz <adelaidemzhang@gmail.com>
2022-10-27 21:58:01 -0400

    add lcov reporter for coverage

--- src/frontend/shortlist/vite.config.js
+++ src/frontend/shortlist/vite.config.js
@@ -6,7 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  test: {},
+  test: {
+    coverage: {
+      reporter:['lcov']
+    },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
