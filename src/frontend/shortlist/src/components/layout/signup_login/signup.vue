38ee9b57d19a30dea329affb064e8abedf091968
Alice Chen <alicechen005@gmail.com>
2022-10-29 13:34:03 -0400

    Added Nav bar

--- src/frontend/shortlist/src/components/layout/signup_login/signup.vue
+++ src/frontend/shortlist/src/components/layout/signup_login/signup.vue
@@ -52,6 +52,31 @@ export default {
 </script>
 
 <template>
+  <div id="nav">
+  <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"> <img src="/favicon.ico" id="favicon_img" ></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="navbar-nav mr-auto">
+            <router-link to="/" class="nav-item nav-link">Home</router-link>
+            <router-link to="/About" class="nav-item nav-link">About</router-link>
+            <router-link to="/preferences" class="nav-link">My Preferences</router-link>
+            <router-link to="/categorize" class="nav-link">School Profiles</router-link>
+            <router-link to="/Contact" class="nav-link">Contact</router-link>
+        </div>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+ </div>
+  <router-view/>
+
   <div id="logo_image_container">
         <img src="/logo.png" id="logo_img" />
   </div>
@@ -120,6 +145,7 @@ input {
   display: block;
   margin-bottom: 10px;
 }
+
 #signup_components_container {
   position: absolute;
   left: 35%;
@@ -133,6 +159,7 @@ input {
   justify-content: center;
   background-color: #bcd6a2; 
 }
+
 #login_components_container {
   position: absolute;
   left: 65%;
@@ -169,5 +196,32 @@ input {
   color: red;
   margin-bottom: 10px;
 }
+
+#favicon_img {
+  width: 30px; 
+}
+
+.d-flex {
+  padding-right: 100px;
+  padding-left: 30px;
+  display: right; 
+}
+
+#nav {
+  padding: 30px;
+  text-align: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: whitesmoke;
+  background: crimson;
+  border-radius: .5rem;
+}
+
 
 </style>
