4a637ab0c41b506f68ba9a9e32894d0327f8f84e
Alice Chen <alicechen005@gmail.com>
2022-10-28 22:22:58 -0400

    Added single sign on with Google

--- src/frontend/shortlist/src/components/layout/signup_login/signup.vue
+++ src/frontend/shortlist/src/components/layout/signup_login/signup.vue
@@ -7,7 +7,9 @@ export default {
       accountName: "",
       password: "",
       passwordVerify: "",
-      alert: ""
+      alert: "",
+      inEditMode: false,
+      localSettings: { ...this.listSettings },
     };
   },
   methods: {
@@ -28,7 +30,10 @@ export default {
         this.alert = error.message;
       });
     },
-  }
+      signupWithSSO() {
+        Userfront.signup({ method: "google" });
+      },
+    },
 };
 </script>
 
@@ -60,6 +65,11 @@ export default {
       </label>
       <button type="submit">Sign up</button>
     </form>
+    <p> or </p>
+    <button @click.prevent="signupWithSSO">Sign up with Google</button>
+  </div>
+  <div id="logo_image_container">
+        <img src="/logo.png" id="logo_img" />
   </div>
 </template>
 
@@ -71,21 +81,42 @@ input {
 }
 #signup_components_container {
   position: absolute;
-  left: 30%;
+  left: 35%;
   top: 50%;
-  height: 300px;
+  height: 400px;
   margin-top: -150px; 
-  width: 300px;
-  margin-left: -100px; 
-  /* width: 70%;
-  height: 100%;
-  margin-top: 200px; 
-  margin-left: 20%; 
-  margin-right: 20%; */
+  width: 400px;
+  margin-left: -80px;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: aquamarine;
+  background-color: #bcd6a2; 
+}
+
+#logo_img {
+  position: absolute;
+  max-width:100%;
+  max-height:100%;
+}
+
+#logo_image_container {
+  position: relative;
+  width: 300px;       
+  height: 300px; 
+  margin-top: 100px;
+  /* border: 5px solid black; */
+  /* left: 15%;
+  top: 50%;
+  height: 10px;
+  margin-top: 100px; 
+  width: 10px;
+  margin-left: 10px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center; */
 }
 
 #alert {
