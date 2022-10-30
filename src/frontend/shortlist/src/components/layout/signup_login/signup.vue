61496b880a4c5b7a857da3beaad30c2df99b4bf3
Alice Chen <alicechen005@gmail.com>
2022-10-30 17:19:30 -0400

    Deleted some redundant css

--- src/frontend/shortlist/src/components/layout/signup_login/signup.vue
+++ src/frontend/shortlist/src/components/layout/signup_login/signup.vue
@@ -3,17 +3,12 @@ export default {
   name: "signup_login",
   data() {
     return {
-      // For log in
-      username_login: "",
-      password_login: "",
-      email_login: "",
       // For sign up
       email_signup: "",
       username_signup: "",
       password_signup: "",
       passwordVerify_signup: "",
       // Alerts
-      alert_login: "",
       alert_signup: "",
     };
   },
@@ -30,22 +25,6 @@ export default {
     },
     signupWithSSO() {
       Userfront.signup({ method: "google" });
-    },
-    loginWithPassword() {
-      this.alert_login = "";
-      if (
-        (this.email_login != "" || this.username_login != "") &&
-        this.password_login != ""
-      ) {
-        this.alert_login = "You have successfully logged in.";
-        return;
-      } else {
-        this.alert_login = "Your Passwords do not match!";
-        return;
-      }
-    },
-    loginWithSSO() {
-      Userfront.login({ method: "google" });
     },
   },
 };
@@ -104,69 +83,12 @@ input {
   justify-content: center;
   background-color: #bcd6a2;
 }
-.login_components_container {
-  position: absolute;
-  left: 65%;
-  top: 50%;
-  height: 450px;
-  margin-top: -150px;
-  width: 450px;
-  margin-right: -80px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #006400;
-}
-#logo_img {
-  position: absolute;
-  max-width: 100%;
-  max-height: 100%;
-}
-#logo_image_container {
-  position: relative;
-  width: 300px;
-  height: 300px;
-  margin-top: 100px;
-}
 #alert_signup {
   color: red;
   margin-bottom: 10px;
 }
-#alert_login {
-  color: red;
-  margin-bottom: 10px;
-}
-#favicon_img {
-  width: 30px;
-}
-#nav {
-  background: whitesmoke;
-  margin: auto;
-}
-#nav {
-  padding: 15px;
-  text-align: center;
-}
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  padding: 15px;
-}
-#nav a.router-link-exact-active {
-  color: whitesmoke;
-  background: crimson;
-  border-radius: 0.5rem;
-  padding: 15px;
-}
-#log_in_sign {
-  position: relative;
-  top: 0;
-}
 #sign_up_sign {
   position: relative;
   top: 0;
-}
-.d-flex {
-  margin-left: auto;
 }
 </style>
