b9533c5c48ef616e6b29c5c2ee6776828ef8002c
Alice Chen <alicechen005@gmail.com>
2022-10-31 18:20:44 -0400

    Deleted cjs file and changed the json files

--- src/frontend/shortlist/cypress.config.js
+++ src/frontend/shortlist/cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    e2e: {
-        specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
+  e2e: {
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
         baseUrl: "http://localhost:4173",
-    },
+  },
 });
