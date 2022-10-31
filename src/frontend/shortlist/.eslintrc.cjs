fc60c38964dc1ea70c5aaa3af0706dc275a236ea
Alice Chen <alicechen005@gmail.com>
2022-10-30 23:00:21 -0400

    checking linter

--- src/frontend/shortlist/.eslintrc.cjs
+++ src/frontend/shortlist/.eslintrc.cjs
@@ -3,17 +3,18 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  rules: {
+    'vue/multi-word-component-names': 0,
+  },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
   ],
-  overrides: [
-    {
-      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-  ],
+  overrides: [{
+    files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
+    extends: ["plugin:cypress/recommended"],
+  }, ],
   ignorePatterns: ["cypress.config.js", "*.css"],
   parserOptions: {
     ecmaVersion: "latest",
