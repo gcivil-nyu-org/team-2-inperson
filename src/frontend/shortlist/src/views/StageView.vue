5a2599915fa3f661f9854b9097ce7f12bbfc4bd0
Adelaide Zhang <adelaidemzhang@gmail.com>
2022-11-01 17:44:33 -0400

    new home with login states; old home>stage

--- src/frontend/shortlist/src/views/StageView.vue
+++ src/frontend/shortlist/src/views/StageView.vue
@@ -1,8 +1,9 @@
 <script>
 import ModalFullScreen from "../components/layout/ModalFullScreen.vue";
+import MaterialIcon from "../components/icons/MaterialIcon.vue";
 
 export default {
-  components: { ModalFullScreen },
+  components: { ModalFullScreen, MaterialIcon },
 };
 </script>
 
@@ -10,7 +11,62 @@ export default {
   <ModalFullScreen visible="true">
     <div>Howdy</div>
   </ModalFullScreen>
-  <div>Background;</div>
+  <div class="home-container">
+      <div class="logo-row">
+        <img src="/logo.png" />
+      </div>    
+      <div class="icons-row">
+        <div class="icon-container">
+          <MaterialIcon src="home" color="rgba(55, 126, 64, .9)" size="50" />
+          <MaterialIcon
+            src="library_add"
+            color="rgba(55, 126, 64, .9)"
+            size="50"
+          />
+          <MaterialIcon src="login" color="rgba(55, 126, 64, .9)" size="50" />
+        </div>
+      </div>
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+.home-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.logo-row {
+  margin-top: 10px;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.icons-row {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.icon-container {
+  width: 600px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 15px;
+  border: 2px rgba(55, 125, 64) solid;
+  box-shadow: 0 0 8px 5px rgba(10, 10, 10, 0.1);
+  padding: 15px;
+  background-color: rgba(55, 125, 64, 0.2);
+}
+
+</style>
