c27d2afdb427c7e557f3de306eea5091c55e6ef8
Daniel Walt <daniel.r.walt@gmail.com>
2022-11-05 19:29:35 +0000

    create handler for dim value get

--- src/backend/server/api/handlers/school_dim_value.py
+++ src/backend/server/api/handlers/school_dim_value.py
@@ -1,2 +1,38 @@
-def school_dim_value():
-    pass
+from __future__ import annotations
+
+from api.handlers.shortlist_request import ShortlistRequest
+from api.models.school import SchoolDimValue
+
+from django.http import HttpRequest
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def school_dim_value(request: HttpRequest):
+    sr = ShortlistRequest(request)
+    if sr.method != "GET":
+        return HttpResponseBadRequest("bad call")
+
+    req_id = request.GET.get("id", None)
+    req_school_id = request.GET.get("schoolId", None)
+    req_dim_id = request.GET.get("dimensionId", None)
+
+    if req_id:
+        try:
+            dim_value = SchoolDimValue.objects.get(pk=req_id)
+            return HttpResponse(dim_value.serializeJson())
+        except Exception:
+            return HttpResponseBadRequest("not found")
+    results = SchoolDimValue.objects
+
+    if req_school_id:
+        results = results.filter(school_id=req_school_id)
+    if req_dim_id:
+        results = results.filter(dim_id=req_dim_id)
+
+    response = []
+    for result in results.all():
+        response.append(result.serializeJson())
+    return HttpResponse("[{}]".format(",".join(response)))
