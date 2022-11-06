830bc15b1a853f1acf5743481a604e2124b87918
yalexublue <yalexu.blue@gmail.com>
2022-11-06 09:50:36 -0500

    added change to school card

--- src/frontend/shortlist/src/components/school/SchoolCard2.vue
+++ src/frontend/shortlist/src/components/school/SchoolCard2.vue
@@ -1,6 +1,4 @@
 <script>
-import Chart from "chart.js/auto";
-
 export default {
   name: "SchoolCard",
   props: ["schoolData"],
@@ -14,46 +12,6 @@ export default {
     swapDetail() {
       this.isDetail = !this.isDetail;
     },
-  },
-  mounted() {
-    const ctx = document.getElementById("myChart");
-    const myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "Dimensions",
-            data: this.schoolData.dimensions,
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
-    myChart;
   },
 };
 </script>
@@ -93,7 +51,10 @@ export default {
         <template v-for="dimension in schoolData.dimensions" :key="dimension">
           <template v-if="!isDetail">
             <template v-if="dimension.simple">
-              <canvas id="myChart" width="400" height="400"></canvas>
+              <div class="school-simple-dim-row">
+                <div class="school-simple-dim-name">{{ dimension.name }}</div>
+                <div class="school-simple-dim-value">{{ dimension.value }}</div>
+              </div>
             </template>
           </template>
           <template v-if="isDetail">
