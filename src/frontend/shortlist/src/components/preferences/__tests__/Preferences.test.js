755bcc96eb3df7977dc792235f0f217e972bd3f5
Alice Chen <alicechen005@gmail.com>
2022-11-05 21:07:34 -0400

    Added an emit test, not done
    I will keep working on this tomorrow.

--- src/frontend/shortlist/src/components/preferences/__tests__/Preferences.test.js
+++ src/frontend/shortlist/src/components/preferences/__tests__/Preferences.test.js
@@ -2,7 +2,9 @@ import { describe, it, expect, vi } from "vitest";
 import { mount } from "@vue/test-utils";
 import { createTestingPinia } from "@pinia/testing";
 import PreferenceDemo from "../PreferenceDemo.vue";
+import PreferenceMultiSelect from "../PreferenceMultiSelect.vue"; 
 
+// Only exists
 describe("PreferenceDemo.vue", () => {
   it("renders with null props", () => {
     const componentWrapper = mount(PreferenceDemo, {
@@ -12,5 +14,40 @@ describe("PreferenceDemo.vue", () => {
     }); 
     expect(componentWrapper.exists()).toBe(true);
   }); 
+}); 
+
+//
+describe("PreferenceMultiSelect.vue", () => {
+  it("renders with props", () => {
+    const componentWrapper = mount(PreferenceMultiSelect, {
+      props: {
+        "question": "string",
+        "modelValue": "string",
+      },
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+      },
+    }); 
+    expect(componentWrapper.exists()).toBe(true);
+  }); 
+
+  it("check updateModelValueEmit", () => {
+    const componentWrapper = mount(PreferenceMultiSelect, {
+      props: {
+        "question": "string",
+        "modelValue": "string",
+      },
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+      },
+    }); 
+
+
+    expect(
+      componentWrapper.emitted(),
+      "properly emitted change event"
+    ).toBeTruthy();
+
+}); 
 
 }); 
