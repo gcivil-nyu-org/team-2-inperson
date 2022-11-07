5e3bec649d6a9d374d7135ac06f19455a0414455
Adelaide Zhang <adelaidemzhang@gmail.com>
2022-11-06 22:41:27 -0500

    testing post requests in stage

--- src/frontend/shortlist/src/views/StageView.vue
+++ src/frontend/shortlist/src/views/StageView.vue
@@ -1,7 +1,37 @@
-<script></script>
+<script>
+import { ref } from "vue"
+import axios from "axios";
+
+export default {
+  data() {
+    let profileData = ref();
+    let prefs = ref();
+    return {
+      profileData,
+      prefs
+    }
+  },
+  mounted() {
+    axios
+      .post('https://api.shortlist.nyc/account/metadata', {},
+      {
+        headers: {'x-shortlist-account':'e44278fec4984522b156cae1a8b3981d'}
+      })
+      .then((response) => {
+        this.profileData = JSON.parse(JSON.stringify(response.data));
+      })
+      .catch((err) => {
+        console.log("AXIOS ERR:", err.response.data);
+      });
+  },
+}
+</script>
 
 <template>
   <h1>Your Work</h1>
+  <p>{{ profileData }}</p>
+
+  <p>{{ prefs }}</p>
 </template>
 
 <style></style>
