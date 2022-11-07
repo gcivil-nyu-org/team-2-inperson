f3929a70386c3657108954f8d763164ac8c55959
Pooja Patel <poojapatel@nyu.edu>
2022-11-06 22:36:19 -0500

    Updated ProfileView

--- src/frontend/shortlist/src/views/ProfileView.vue
+++ src/frontend/shortlist/src/views/ProfileView.vue
@@ -1,61 +1,17 @@
 <script>
-//import Vue from "vue";
-import axios from "axios";
-//import VueAxios from "vue-axios";
-//import { getAPI } from "../axios-api";
-
 import UserDetails from "../components/layout/profile/UserDetails.vue";
 import UserPreference from "../components/layout/profile/UserPreference.vue";
 import UserRelations from "../components/layout/profile/UserRelations.vue";
 
-//Vue.useAttrs(VueAxios.axios);
 export default {
-  //   data() {
-  //     return {
-  //       APIData: [],
-  //     };
-  //   },
   components: {
     UserDetails,
     UserPreference,
     UserRelations,
   },
-  //   created() {
-  //     getAPI
-  //       .get("/account/")
-  //       .then((response) => {
-  //         this.APIData = response.data;
-  //         console.log(response.data);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //   },
-  //   mounted() {
-  //     Vue.axios.get("http://manage.shortlist.nyc/admin/api/account/")
-  //     .then((resp) => {
-  //         console.warn(resp);
-  //       });
-  //   },
-
-  mounted() {
-    const config = {
-      headers: {
-        email: "poojapatel@nyu.edu",
-      },
-    };
-    const url = "https://api.shortlist.nyc/account/metadata";
-    axios
-      .post(url, config)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log("AXIOS ERR:", err);
-      });
-  },
 };
 </script>
+
 <template>
   <main>
     <div class="profile">
@@ -69,6 +25,7 @@ export default {
     </div>
   </main>
 </template>
+
 <style scoped>
 .profile {
   height: 100%;
@@ -79,15 +36,12 @@ export default {
   grid-column-gap: 0px;
   grid-row-gap: 0px;
 }
-
 .statusdetails {
   grid-area: 1 / 1 / 2 / 2;
   padding: 5%;
-  background-color: darkgoldenrod;
 }
 .prefrel {
   grid-area: 1 / 3 / 2 / 4;
   padding: 5%;
-  background-color: darkgoldenrod;
 }
 </style>
