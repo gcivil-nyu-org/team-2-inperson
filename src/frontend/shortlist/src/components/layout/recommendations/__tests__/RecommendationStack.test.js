1ef0adc3ee291fdfc3ec2f851cec0abf4c827258
Alice Chen <alicechen005@gmail.com>
2022-11-06 19:43:12 -0500

    Added 3 tests and comments about where to add API calls

---
+++ src/frontend/shortlist/src/components/layout/recommendations/__tests__/RecommendationStack.test.js
@@ -0,0 +1,72 @@
+import { describe, it, expect, vi } from "vitest";
+import { mount } from "@vue/test-utils";
+import { createTestingPinia } from "@pinia/testing";
+import RecommendationStack from "../RecommendationStack.vue";
+// import { recommendation } from "../../../server/api/models/recommendation.py"; 
+
+describe("RecommendationStack.vue", () => {
+  it("renders with null props", () => {
+    const componentWrapper = mount(RecommendationStack, {
+      props: {},
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+      },
+    });
+    // exists
+    expect(componentWrapper.exists()).toBe(true);
+  });
+
+  // TODO()
+  it("renders with well-formed props", () => {
+    const componentWrapper = mount(RecommendationStack, {
+      props: {
+        /*
+        TODO(Need to add recommendaton properties here to match API calls)
+        
+        */
+      },
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+      },
+    });
+    // exists
+    expect(componentWrapper.exists()).toBe(true);
+  });
+
+  // Component Testing
+  it("check if reco-stack-container exists", () => {
+    const componentWrapper = mount(RecommendationStack, {
+      props: {
+        /*
+        TODO(Need to add recommendaton properties here to match API calls)
+        */
+      },
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+      },
+    });
+    // exists
+    const reco_stack_container = componentWrapper.find(".reco-stack-container")
+    expect(reco_stack_container.exists()).toBe(true);
+  });
+
+  it("check if reco-stack-grid-position exists", () => {
+    const componentWrapper = mount(RecommendationStack, {
+      props: {
+        /*
+        TODO(Need to add recommendaton properties here to match API calls)
+        */
+      },
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+      },
+    });
+    // exists. This does not exist in the file. 
+    const reco_stack_grid_positionr = componentWrapper.find(".reco-stack-grid-position")
+    expect(reco_stack_grid_positionr.exists()).toBe(false);
+  });
+
+
+
+}); 
+
