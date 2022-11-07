10ce3a9acbc44104e0fbe0e8c1c1f07bcdfff40c
Adelaide Zhang <adelaidemzhang@gmail.com>
2022-11-06 22:39:28 -0500

    add mitt to npm for event emit/listen

--- src/frontend/shortlist/src/main.js
+++ src/frontend/shortlist/src/main.js
@@ -2,14 +2,19 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import mitt from 'mitt';
 
 import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
 const pinia = createPinia();
+const emitter = mitt();
+
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
+app.config.globalProperties.emitter = emitter
+
 app.mount("#app");
