ef3e1cec49450086ef75feeac63fe5b8a8bec0ea
Pooja Patel <poojapatel@nyu.edu>
2022-11-07 20:23:08 -0500

    profile pref style

--- src/frontend/shortlist/src/components/layout/profile/UserPreference.vue
+++ src/frontend/shortlist/src/components/layout/profile/UserPreference.vue
@@ -8,39 +8,10 @@ export default {
 </script>
 
 <template>
-  <PreferenceDemo />
+  <PreferenceDemo class="pref" />
 </template>
 <style scoped>
-.prefprofileform {
-  padding: 10px;
-  width: 100%;
-}
-.prefprofiletitle {
-  width: 100%;
-  font-family: "Cabin Sketch", cursive;
-  font-weight: bold;
-  font-size: 2.5rem;
-  padding-bottom: None;
-  line-height: 2rem;
-  color: #067418;
-}
-.pref-q-name {
-  padding: 10px;
-  width: 50%;
-  font-family: "Klee One", cursive;
-  font-weight: bold;
-}
-.profilefields {
-  width: 50%;
-  background: #ebf3e6;
-  border: 1px solid #008037;
-  border-radius: 5px;
-  color: black;
-  font-weight: bold;
-  font-size: 15px;
-  font-family: "Aleo", serif;
-  outline: none;
-  transition: border-color 0.2s;
-  margin-bottom: 20px;
+.pref {
+  padding: 9px;
 }
 </style>
