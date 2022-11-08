4b87001d2dc4314a2d24a0909aac973f5b55a65a
Daniel Walt <daniel.r.walt@gmail.com>
2022-11-08 21:19:06 +0000

    fix endpoints

--- src/backend/server/authentication/urls.py
+++ src/backend/server/authentication/urls.py
@@ -14,18 +14,18 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginAPIView.as_view(), name="login"),
-    path("logout/", LogoutAPIView.as_view(), name="logout"),
-    path("email-verify/", VerifyEmail.as_view(), name="email-verify"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register", RegisterView.as_view(), name="register"),
+    path("login", LoginAPIView.as_view(), name="login"),
+    path("logout", LogoutAPIView.as_view(), name="logout"),
+    path("email-verify", VerifyEmail.as_view(), name="email-verify"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path(
-        "request-reset-email/",
+        "request-reset-email",
         RequestPasswordResetEmail.as_view(),
         name="request-reset-email",
     ),
     path(
-        "password-reset/<uidb64>/<token>/",
+        "password-reset/<uidb64>/<token>",
         PasswordTokenCheckAPI.as_view(),
         name="password-reset-confirm",
     ),
