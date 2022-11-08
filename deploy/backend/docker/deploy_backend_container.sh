de82552f616a6e89f44db94f781b141542103253
Daniel Walt <daniel.r.walt@gmail.com>
2022-11-08 18:02:24 +0000

    deploy script modification for merge env

--- deploy/backend/docker/deploy_backend_container.sh
+++ deploy/backend/docker/deploy_backend_container.sh
@@ -2,7 +2,7 @@
 ACCESS_KEY=$(aws configure get aws_access_key_id --profile=shortlist)
 SECRET_KEY=$(aws configure get aws_secret_access_key --profile=shortlist)
 
-cp ~/.shortlist/.env_merge .
+cp ~/.shortlist/.env_merge .env
 
 aws ecr get-login-password --profile=shortlist | docker login --username AWS --password-stdin 147551946776.dkr.ecr.us-east-1.amazonaws.com/shortlist-backend
 
@@ -11,6 +11,6 @@ docker build . \
   --build-arg AWS_SECRET_ACCESS_KEY=$SECRET_KEY \
   -t 147551946776.dkr.ecr.us-east-1.amazonaws.com/shortlist-backend:latest \
 
-rm -rf .env*
+rm -rf .env
 
 docker push 147551946776.dkr.ecr.us-east-1.amazonaws.com/shortlist-backend:latest
