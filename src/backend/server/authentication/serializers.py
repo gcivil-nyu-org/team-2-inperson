4500b520a157dd111f7fb14b3de1c226aed68c58
Vaibhav Jain <vj92219@gmail.com>
2022-11-13 22:41:39 -0500

    added request-resend-email endpoint (#164)
    * add redirect link to verify-email api
    
    * merged account_create from api app to register endpoint on authentication app
    
    * change recommendation model __str__
    
    * added request-resend-email endpoint
    
    * added button to the verification email
    
    * reformat via black linter
    
    * reformat via black linter
    
    * removed unused package
    
    * deleted migrations

--- src/backend/server/authentication/serializers.py
+++ src/backend/server/authentication/serializers.py
@@ -40,6 +40,13 @@ class EmailVerificationSerializer(serializers.ModelSerializer):
         fields = ["token"]
 
 
+class ResendEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(min_length=2)
+
+    class Meta:
+        fields = ["email"]
+
+
 class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255, min_length=3)
     password = serializers.CharField(max_length=68, min_length=6, write_only=True)
