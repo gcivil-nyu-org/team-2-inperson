c0265cae76f95a3d9b5a86eb9043f39b46b65c3d
Alice Chen <alicechen005@gmail.com>
2022-11-14 11:30:41 -0500

    Added tests that do not involve login and signup

---
+++ src/frontend/shortlist/src/components/layout/signup_login/__tests__/LoggedOut.test.js
@@ -0,0 +1,16 @@
+import { describe, it, expect, vi } from "vitest";
+import { mount } from "@vue/test-utils";
+import LoggedOut from "../LoggedOut.vue";
+import { createTestingPinia } from "@pinia/testing";
+
+describe("LoggedOut.vue", () => {
+  // Check if LoggedOut exists
+  it("Renders with null prop", () => {
+    const componentWrapper = mount(LoggedOut, {
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+      },
+    });
+    expect(componentWrapper.exists()).toBe(true);
+  });
+});
