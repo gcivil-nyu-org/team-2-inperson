9dd277026c9020a399087b2bb072963cc0c02d67
yalexublue <78865690+yalexublue@users.noreply.github.com>
2022-11-24 09:11:16 -0500

    Student - Accept Invitation Setup Page (#191)
    * Added new vue file and router setup
    
    * added extra components
    
    * css
    
    * basic done, left emit
    
    * Revert to sign up view
    
    * page edit
    
    * Finalize change
    
    * Fully updated with all emits

---
+++ src/frontend/shortlist/src/views/InvitedView.vue
@@ -0,0 +1,19 @@
+<script>
+import Signup from "../components/layout/profile/UserDetailsInvite.vue"
+export default {
+  emits: ["appAccountAcceptInvite"],
+  components: {
+    Signup,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Signup
+      @appAccountSignup="(p) => this.$emit('appAccountAcceptInvite', p)"
+    />
+  </div>
+</template>
+
+<style scoped></style>
