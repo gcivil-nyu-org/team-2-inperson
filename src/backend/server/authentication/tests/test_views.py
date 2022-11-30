4b15ef7a3e4363cbcc51857b690a62d67f0f4730
Daniel Walt <daniel.r.walt@gmail.com>
2022-11-30 13:40:16 -0500

    lint

--- src/backend/server/authentication/tests/test_views.py
+++ src/backend/server/authentication/tests/test_views.py
@@ -3,6 +3,7 @@ from ..models import User
 
 from unittest.mock import patch
 
+
 class TestViews(TestSetUp):
     def test_user_cannot_register_with_no_data(self):
         res = self.client.post(self.register_url)
@@ -14,14 +15,25 @@ class TestViews(TestSetUp):
             self.assertEqual(res.data["email"], self.user_data["email"])
             self.assertEqual(res.data["username"], self.user_data["username"])
             self.assertEqual(res.status_code, 201)
+
+            # Email Testing
             self.assertTrue(mocked_email.call_count == 1, "django call to send email")
-            
-            # first call, first positional 
+
+            # first call, first positional
             email_call = mocked_email.mock_calls[0].args[0]
 
             self.assertTrue(isinstance(email_call, dict), "send email called with dict")
-            self.assertEqual(email_call["email_subject"], "Verify your email", "email subject matches")
-            self.assertEqual(email_call["to_email"], self.user_data["email"], "email to address matches")
+            
+            self.assertEqual(
+                email_call["email_subject"],
+                "Verify your email",
+                "email subject matches",
+            )
+            self.assertEqual(
+                email_call["to_email"],
+                self.user_data["email"],
+                "email to address matches",
+            )
             self.assertTrue("email_body" in email_call, "email body present")
 
     def test_user_cannot_login_with_unverified_email(self):
