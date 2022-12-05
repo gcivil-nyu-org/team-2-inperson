cb9a244a0130549a78c1ec8a3a8ed21b635f35c4
Alice Chen <alicechen005@gmail.com>
2022-12-04 19:35:10 -0500

    changed one function

--- src/frontend/shortlist/src/App.vue
+++ src/frontend/shortlist/src/App.vue
@@ -10,7 +10,7 @@ const apiClient = new ShortlistApi("https://api.shortlist.nyc");
 const router = useRouter();
 const appSessionStore = sessionStore();
 
-function showSchoolAsAccepted(payload) {
+function markSchoolAsAccepted(payload) {
   console.log(payload);
   let apiReq = {
     recoID: payload.recoID,
@@ -145,7 +145,7 @@ function appAddStudent(payload) {
       @appAccountUpdateName="appAccountUpdateName"
       @logoutEvent="appLogout"
       @addStudent="appAddStudent"
-      @markSchoolAsAccepted="showSchoolAsAccepted"
+      @markSchoolAsAccepted="markSchoolAsAccepted"
     />
   </div>
 </template>
