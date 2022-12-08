c6d581dba0030f9ea344bc838ee37ad495708160
Alice Chen <alicechen005@gmail.com>
2022-12-08 14:18:36 -0500

    Added new css to ResetPasswordView

--- src/frontend/shortlist/src/views/ResetPasswordView.vue
+++ src/frontend/shortlist/src/views/ResetPasswordView.vue
@@ -1,22 +1,60 @@
-<script></script>
+<script>
+  export default {
+    name: "ResetPasswordView",
+    data() {
+      return {
+        isMatched: false,
+        notMatchingMessage: "",
+        passwordAlert: "",
+        validation: true,
+        newPassword: "",
+        newConfirmPassword: "",
+      };
+    },
+    methods: {
+      validatePassword() {
+      let passwordPattern = new RegExp(
+        "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)"
+      );
+      if (this.form.newPassword.length < 8) {
+        this.passwordAlert = "Minimum length is 8 for password!";
+        return false;
+      }
+      if (this.form.newPassword.length > 15) {
+        this.passwordAlert = "Maximum length is 15 for password!";
+        return false;
+      }
+      if (!passwordPattern.test(this.form.newPassword)) {
+        this.passwordAlert =
+          "Invalid Password. At least 1 digit, 1 lower case, 1 upper case, and 1 special required.";
+        return false;
+      }
+      return true;
+    },
+    validateConfirmPassword() {
+      return this.form.newConfirmPassword == this.form.newConfirmPassword;
+    },
+    }
+  }
+</script>
 
 <template>
   <main style="margin: auto">
     <div class="form-container">
       <div>
-        <label>Current Password</label>
+        <label id = "instructions" >Current Password</label>
         <input type="text" />
       </div>
       <div>
-        <label>New Password</label>
+        <label id = "instructions" >New Password</label>
         <input type="text" />
       </div>
       <div>
-        <label>Confirm New Password</label>
+        <label id = "instructions">Confirm New Password</label>
         <input type="text" />
       </div>
 
-      <button class="btn btn-outline-dark" @click.prevent="submitSignupForm">
+      <button id ="big-instructions" class="btn btn-outline-dark" @click.prevent="submitSignupForm">
         Submit
       </button>
     </div>
@@ -26,7 +64,8 @@
 <style scoped>
 .form-container {
   position: absolute;
-  margin: auto;
+  margin-left: 30%;
+  margin-top: 4%;
   padding: 5%;
   display: flex;
   align-items: center;
@@ -43,5 +82,15 @@ input {
   box-sizing: border-box;
   border: 1px solid #008037;
   border-radius: 5px;
+}
+#big-instructions {
+  font-size: 28px;
+  font-weight: 500;
+  font-family: "Cabin Sketch", cursive;
+}
+#instructions{
+  font-size: 18px;
+  font-weight: 500;
+  font-family: "Cabin Sketch", cursive;
 }
 </style>
