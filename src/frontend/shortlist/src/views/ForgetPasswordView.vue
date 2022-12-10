0f7a8553ce36dd873e9523cf92e7df95993ecce4
Simao (Alice) Chen <67714084+simaoalicechen@users.noreply.github.com>
2022-12-10 09:24:11 -0500

    Forget password UI (#227)
    * added forget password button, setup the router
    
    * Completed Forget password view ui
    
    * added forget to the routers
    
    * modified the router
    
    * modified validation logic
    
    * fixed two linter issues
    
    * all frontend and axios call done
    
    * all functionality working

---
+++ src/frontend/shortlist/src/views/ForgetPasswordView.vue
@@ -0,0 +1,19 @@
+<script>
+import Forget from "../components/layout/signup_login/Forget.vue";
+export default {
+  name: "ForgetPasswordView",
+  components: {
+    Forget,
+  },
+};
+</script>
+
+<template>
+  <form>
+    <div>
+      <Forget />
+    </div>
+  </form>
+</template>
+
+<style scoped></style>
