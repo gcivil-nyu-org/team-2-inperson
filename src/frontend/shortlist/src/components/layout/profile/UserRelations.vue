e817873e5da11a0260c3fc25fe1b201968d65c3a
Pooja Patel <poojapatel@nyu.edu>
2022-12-11 17:55:36 -0500

    updated relations

--- src/frontend/shortlist/src/components/layout/profile/UserRelations.vue
+++ src/frontend/shortlist/src/components/layout/profile/UserRelations.vue
@@ -46,48 +46,14 @@ export default {
 </script>
 <template>
   <main>
-    <label class="relprofiletitle">Student Accounts</label>
-    <div>
-      <span class="studentlistpanel">
-        <span class="student">
-          <h2>
-            <img
-              src="/default-student-profile.png"
-              alt="Avatar"
-              class="studentimg"
-            />
-          </h2>
-          <h3>Student 1</h3>
-          <p>
-            <label class="fileidlabel">Student1 Name</label>
-            <label class="fileidlabel">Student1 Email</label>
-          </p>
-        </span>
-      </span>
-      <span class="studentlistpanel">
-        <span class="student">
-          <h2>
-            <img
-              src="/default-student-profile.png"
-              alt="Avatar"
-              class="studentimg"
-            />
-          </h2>
-          <h3>Student 2</h3>
-          <p>
-            <label class="fileidlabel">Student2 Name</label>
-            <label class="fileidlabel">Student2 Email</label>
-          </p>
-        </span>
-      </span>
-    </div>
+    <label class="relprofiletitle">Invite Others</label>&nbsp;
     <span>
       <input
         type="email"
         class="emailInput"
-        placeholder="Your child's email"
+        placeholder="Enter Email"
         v-model="newEmail"
-      />
+      />&nbsp;
       <button
         type="button"
         @click="this.sendInvite"
@@ -99,107 +65,38 @@ export default {
   </main>
 </template>
 <style scoped>
+main {
+  height: 40px;
+  background-color: #000000;
+  padding-left: 35%;
+}
 .relprofiletitle {
   font-family: "Cabin Sketch", cursive;
   font-weight: bold;
-  font-size: 2.5rem;
-  padding-bottom: 10px;
+  font-size: 1.5rem;
+  padding: 0px 10px 10px 10px;
   line-height: 2rem;
-  color: #067418;
+  color: #ffffff;
 }
 
-.studentimg {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
 .studentupdateimg {
   vertical-align: middle;
   width: 25px;
   height: 25px;
   border-radius: 50%;
-}
-.fileidlabel {
-  color: rgb(0, 0, 0);
-  font-size: small;
-  padding: 1%;
-  float: left;
-  margin-right: 5px;
+  margin: 1px;
 }
 
-body {
-  background: #262a2b;
-}
-
-.studentlistpanel {
-  padding: 0;
-  margin: 50px auto;
-  list-style: none;
-  text-align: center;
-}
-.studentlistpanel .student {
-  display: inline-block;
-  width: 20%;
-  height: 10px;
-  min-width: 100px;
-  max-width: 160px;
-  padding: 5px 5px 5px 5px;
-  position: relative;
-  vertical-align: top;
-  margin: 10px;
-  font-family: "helvetica", san-serif;
-  min-height: 15vh;
-  background: #bad1be;
-  border: 1px solid #000000;
-  text-align: left;
-}
-.studentlistpanel .student h2 {
-  margin: 0;
-  position: absolute;
-  opacity: 0.6;
-  top: 40px;
-  right: 5px;
-  transition: all 0.3s ease-in-out;
-}
-.studentlistpanel .student h3 {
-  font-size: 20px;
-  color: #000000;
-  margin-bottom: 5px;
-}
-.studentlistpanel .student p {
-  font-size: 16px;
-  line-height: 18px;
-  color: #b7b7b7;
-  margin-top: 5px;
-}
-.studentlistpanel .student:hover h2 {
-  top: 20px;
-  opacity: 1;
-}
-
-.studentlistpanel .student:before {
-  content: "";
-  position: absolute;
-  top: -2px;
-  left: -2px;
-  right: -2px;
-  bottom: -2px;
-  z-index: -1;
-  background: #fff;
-  transform: skew(2deg, 2deg);
-}
-.studentlistpanel .student:after {
-  content: "";
-  position: absolute;
-  width: 40%;
-  height: 100%;
-  left: 0;
-  top: 0;
-  background: rgba(255, 255, 255, 0.02);
-}
-.studentlistpanel .student:nth-child(1):before {
-  background: #083201;
-  background: -webkit-linear-gradient(to right, #bdc6c0, #72df5f);
-  background: linear-gradient(to right, #000000, #57d640);
+.emailInput {
+  background: #ebf3e6;
+  border: 1px solid #008037;
+  border-radius: 5px;
+  color: black;
+  font-weight: bold;
+  font-size: 15px;
+  font-family: "Aleo", serif;
+  outline: none;
+  transition: border-color 0.2s;
+  margin-bottom: 0px;
 }
 </style>
