ed24dc035f903395cfa942be512cdf39ad311074
Pooja Patel <poojapatel@nyu.edu>
2022-12-12 19:36:24 -0500

    added developer card template

---
+++ src/frontend/shortlist/src/components/layout/about/DeveloperCard.vue
@@ -0,0 +1,179 @@
+<script></script>
+<template>
+  <main>
+    <card>
+      <div class="border">
+        <img class="devimg" src="/AdelaideZhangDevShortlist.jpeg" />
+      </div>
+      <h2>Adelaide Zhang</h2>
+      <h3>Software Engineer</h3>
+      <div class="social-menu">
+        <ul>
+          <li>
+            <a
+              href="https://mail.google.com/mail/?view=cm&source=mailto&to=amz316@nyu.edu&body=Hello%20Adelaide,&su=Shortlist%20Support"
+              target="blank"
+              ><img class="socialimg" src="/email.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://github.com/adelaidemz" target="blank"
+              ><img class="socialimg" src="/github.png"
+            /></a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/adelaide-zhang-72306019/"
+              target="blank"
+              ><img class="socialimg" src="/linkedin.png"
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </card>
+    <card>
+      <div class="border">
+        <img class="devimg" src="/Simao(Alice)ChenDevShortlist.jpg" />
+      </div>
+      <h2>Alice Chen</h2>
+      <h3>Software Engineer</h3>
+      <div class="social-menu">
+        <ul>
+          <li>
+            <a
+              href="https://mail.google.com/mail/?view=cm&source=mailto&to=sc4916@nyu.edu&body=Hello%20Alice,&su=Shortlist%20Support"
+              target="blank"
+              ><img class="socialimg" src="/email.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://github.com/simaoalicechen" target="blank"
+              ><img class="socialimg" src="/github.png"
+            /></a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/simao-alice-chen-98377711a/"
+              target="blank"
+              ><img class="socialimg" src="/linkedin.png"
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </card>
+    <card>
+      <div class="border">
+        <img class="devimg" src="/DanielWaltDevShortlist.jpeg" />
+      </div>
+      <h2>Daniel Walt</h2>
+      <h3>Software Engineer</h3>
+      <div class="social-menu">
+        <ul>
+          <li>
+            <a
+              href="https://mail.google.com/mail/?view=cm&source=mailto&to=dw2789@nyu.edu&body=Hello%20Daniel,&su=Shortlist%20Support"
+              target="blank"
+              ><img class="socialimg" src="/email.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://github.com/waltage" target="blank"
+              ><img class="socialimg" src="/github.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/daniel-walt/" target="blank"
+              ><img class="socialimg" src="/linkedin.png"
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </card>
+    <card>
+      <div class="border">
+        <img class="devimg" src="/PoojaPatelDevShortlist.jpg" />
+      </div>
+      <h2>Pooja Patel</h2>
+      <h3>Software Engineer</h3>
+      <div class="social-menu">
+        <ul>
+          <li>
+            <a
+              href="https://mail.google.com/mail/?view=cm&source=mailto&to=poojapatel@nyu.edu&body=Hello%20Pooja,&su=Shortlist%20Support"
+              target="blank"
+              ><img class="socialimg" src="/email.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://github.com/Pooja-Codes" target="blank"
+              ><img class="socialimg" src="/github.png"
+            /></a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/pooja-patel-here/"
+              target="blank"
+              ><img class="socialimg" src="/linkedin.png"
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </card>
+    <card>
+      <div class="border">
+        <img class="devimg" src="/VaibhavJainDegShortlist.jpg" />
+      </div>
+      <h2>Vaibhav Jain</h2>
+      <h3>Software Engineer</h3>
+      <div class="social-menu">
+        <ul>
+          <li>
+            <a
+              href="https://mail.google.com/mail/?view=cm&source=mailto&to=vj2075@nyu.edu&body=Hello%20Vaibhav,&su=Shortlist%20Support"
+              target="blank"
+              ><img class="socialimg" src="/email.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://github.com/jainsvaibhav" target="blank"
+              ><img class="socialimg" src="/github.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/vj92219/" target="blank"
+              ><img class="socialimg" src="/linkedin.png"
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </card>
+    <card>
+      <div class="border">
+        <img class="devimg" src="/YeXuDevShortlist.jpg" />
+      </div>
+      <h2>Ye Xu</h2>
+      <h3>Software Engineer</h3>
+      <div class="social-menu">
+        <ul>
+          <li>
+            <a
+              href="https://mail.google.com/mail/?view=cm&source=mailto&to=yx2534@nyu.edu&body=Hello%20Ye,&su=Shortlist%20Support"
+              target="blank"
+              ><img class="socialimg" src="/email.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://github.com/yalexublue" target="blank"
+              ><img class="socialimg" src="/github.png"
+            /></a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/ye-xu-72703a46/" target="blank"
+              ><img class="socialimg" src="/linkedin.png"
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </card>
+  </main>
+</template>
