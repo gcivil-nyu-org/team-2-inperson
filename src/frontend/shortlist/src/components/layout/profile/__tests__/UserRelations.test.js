aeb0eee4db2c75c42b478f7c5dc0b83835660164
yalexublue <yalexu.blue@gmail.com>
2022-12-12 21:37:11 -0500

    User Relation test finished

---
+++ src/frontend/shortlist/src/components/layout/profile/__tests__/UserRelations.test.js
@@ -0,0 +1,26 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import UserRelations from "../UserRelations.vue";
+
+describe("UserRelations.vue", () => {
+  it("renders with null props", () => {
+    const componentWrapper = mount(UserRelations, {});
+    // exists
+    expect(componentWrapper.exists());
+  });
+  it("validate email fine", () => {
+    const componentWrapper = mount(UserRelations, {});
+    componentWrapper.vm.newEmail = "as@abc.com";
+    componentWrapper.vm.validateEmail();
+    expect(componentWrapper.vm.validationresult).toBe(true);
+  });
+  it("sends email fine", async () => {
+    const componentWrapper = mount(UserRelations, {});
+    const inviteButton = componentWrapper.find("#send-invite-button");
+    expect(inviteButton.exists()).toBe(true);
+    componentWrapper.vm.newEmail = "ab@abc.com"
+    inviteButton.trigger("click");
+    expect(componentWrapper.vm.inviteSented).toBe(true);
+  });
+
+});
