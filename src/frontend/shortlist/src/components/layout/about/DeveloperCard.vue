b79a69ee2d2404eac1fb89a316aedd365d59c714
Pooja Patel <poojapatel@nyu.edu>
2022-12-12 19:36:44 -0500

    added developer card css

--- src/frontend/shortlist/src/components/layout/about/DeveloperCard.vue
+++ src/frontend/shortlist/src/components/layout/about/DeveloperCard.vue
@@ -177,3 +177,115 @@
     </card>
   </main>
 </template>
+<style scoped>
+main {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 13rem;
+  padding: 1rem 1rem;
+  border-radius: 1.4rem;
+  background: linear-gradient(-45deg, #351b07ba, rgb(22, 76, 7));
+  animation: 1s ease-out 0s 1 slideInFromLeft;
+}
+@keyframes slideInFromLeft {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+.devimg {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  border: 4px solid #1d1e22;
+}
+
+.border {
+  width: 154px;
+  height: 154px;
+  padding: 1px;
+  margin-bottom: 1rem;
+  border-radius: 50%;
+  background: linear-gradient(-45deg, #ffffff, #000000);
+}
+
+h2,
+h3 {
+  line-height: 1.1;
+  margin: 0;
+  font-weight: 500;
+}
+
+h2 {
+  margin-bottom: 0.25rem;
+  font-size: 1rem;
+  color: rgba(255, 255, 255, 0.96);
+}
+
+h3 {
+  margin-bottom: 1rem;
+  font-size: 0.75rem;
+  color: rgba(255, 255, 255, 0.66);
+}
+
+.social-menu {
+  width: 7rem;
+  height: 2.25rem;
+  margin-top: 1rem;
+  transform: translate(50%, -50%);
+}
+
+.social-menu ul li :hover {
+  color: #fff;
+}
+.social-menu ul {
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  right: 45px;
+  top: 0px;
+}
+.social-menu ul li a {
+  display: block;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #fff;
+  text-align: center;
+  transition: 0.6s;
+  box-shadow: 0 5px 4px rgba(0, 0, 0, 0.5);
+  margin: 3px;
+}
+
+.social-menu ul li a:hover {
+  transform: translate(0, -10%);
+}
+
+.social-menu ul li:nth-child(1) a:hover {
+  background-color: #c4b4ff;
+}
+.social-menu ul li:nth-child(2) a:hover {
+  background-color: rgba(252, 246, 82, 0.829);
+}
+.social-menu ul li:nth-child(3) a:hover {
+  background-color: #60a7e9;
+}
+
+.socialimg {
+  width: 65%;
+  height: 65%;
+  margin-top: 0.4rem;
+  margin-left: 0.1rem;
+}
+</style>
