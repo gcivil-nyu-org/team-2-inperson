7add10cd490adbaa50b055fe35a2174990b17f36
Pooja Patel <87775823+Pooja-Codes@users.noreply.github.com>
2022-12-13 09:51:10 -0500

    Shortlist About, Support & Developers (#245)
    * initial
    
    * Shortlist Developers
    
    * added developer card template
    
    * added developer card css
    
    * Added About script
    
    * Added about template
    
    * Added about style

--- src/frontend/shortlist/src/views/AboutView.vue
+++ src/frontend/shortlist/src/views/AboutView.vue
@@ -1,56 +1,93 @@
-<script></script>
+<script>
+import DeveloperCard from "../components/layout/about/DeveloperCard.vue";
+export default {
+  components: {
+    DeveloperCard,
+  },
+};
+</script>
 
 <template>
-  <div class="container">
-    <h1 class="bigtitle">About Shortlist™</h1>
-  </div>
-  <div class="content-container">
-    <p class="contents">
-      Shortlist™ is an education web application specializes in matching middle
-      school students or parents with their desired high schools in NYC metro
-      area 🌃. Different from the traditional "prune and search" edu app that
-      has been existed in the past few decades, Shortlist™ feeds users around
-      400 NYC public high schools using personalized algorithms and swipping
-      mechanism. It allows users to categorize the schools in three different
-      lists or "🚮" certain schools to minimize distractions. Each school card
-      contains personalized and relevant information. Users will be able to view
-      dimensions of the info that matters the most to them, based on their
-      preference settings before they start swipping. Users can also click on
-      each of the school cards to view more in-depth information.
-    </p>
-    <p class="contents">
-      Each list can contain up to four different schools and users can
-      personalize the lists' name, style, and priority. Once users are satisfied
-      with their selections in a list, they can share such a list to their
-      friends and families so that they can discuss which schools are the best
-      options for their high school experience. What could be more exciting than
-      talking about which high school to go together with your besties 👭👬 in a
-      few months?
-    </p>
-    <p class="contents">
-      Does this sound fun and familiar? We certainly hope so 😉 We are here to
-      help hone your intuition and lead you to your destiny for the next four
-      (best) years 💘.
-    </p>
-  </div>
+  <main>
+    <div class="ShortlistLogo">
+      <img src="/short-list-logo-light-navbar.png" />
+    </div>
+    <div class="container">
+      <p class="contents">
+        Shortlist™ is an education web application specializes in matching
+        middle school students or their parents with their desired high schools
+        in NYC. Different from the traditional “prune and search” edu app that
+        has been existed in the past few decades, Shortlist™ feeds users around
+        400 NYC public high schools using personalized algorithms and
+        drag-n-drop mechanism. It allows users to quickly make decision to keep
+        or discard recommended schools. Each school card contains personalized
+        and relevant information. Users can also click on each of the school
+        cards to view more in-depth information.
+      </p>
+      <p class="contents">
+        Each list can contain up to four different schools and users can
+        personalize the lists’ name, style, and priority. Once users are
+        satisfied with their selections in a list, they can share such a list to
+        their friends and families so that they can discuss which schools are
+        the best options for their high school experience. What could be more
+        exciting than talking about which high school to go together with your
+        besties in a few months?
+      </p>
+      <p class="contentslast">
+        Does this sound fun and familiar? We certainly hope so We are here to
+        help hone your intuition and lead you to your destiny for the next four
+        (best) years 💚.
+      </p>
+    </div>
+    <div>
+      <h1 class="teamtitle">- - Team Shortlist - -</h1>
+    </div>
+    <DeveloperCard
+      style="
+        background: linear-gradient(-65deg, #7bdf8d, #525854);
+        padding: 13px;
+      "
+    ></DeveloperCard>
+  </main>
 </template>
 
 <style>
-.container {
-  margin-left: 40%;
+.main {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(4, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+
+  background: linear-gradient(-65deg, #7bdf8d, #525854);
 }
-.content-container {
-  margin: 2% 2% 2% 2%;
+.ShortlistLogo {
+  width: 10%;
 }
-.bigtitle {
-  font-size: 50px;
-  font-weight: 500;
-  font-family: "Cabin Sketch", cursive;
+.ShortlistLogo img {
+  margin-left: auto;
+  margin-right: auto;
+  left: 50%;
+  width: 190%;
+  transform: translate(220%);
 }
 .contents {
-  font-size: 25px;
-  font-weight: 200;
-  color: #008037;
-  font-family: "Cabin Sketch", cursive;
+  font-family: "Aleo", sans-serif;
+  text-align: justify;
+}
+.contentslast {
+  font-family: "Aleo", sans-serif;
+  text-align: justify;
+  margin-bottom: 0px;
+}
+.teamtitle {
+  font-family: "Klee One";
+  color: rgb(62, 61, 61);
+  font-size: 1.5rem;
+  margin: 0%;
+  text-align: center;
+  padding: 0%;
+  padding-bottom: 2px;
+  font-weight: bold;
 }
 </style>
